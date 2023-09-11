17dbb2c957aa76acb40963293e517f8f5fa93665
thenatural86 <philip.smalls86@gmail.com>
2023-09-11 16:56:37 -0400

    increase and decrease functionality

--- vite-starter/src/components/CartItem.jsx
+++ vite-starter/src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ChevronDown, ChevronUp } from '../icons'
-import { increase, removeItem } from '../features/cart/cartSlice'
+import { increase, removeItem, decrease } from '../features/cart/cartSlice'
 import { useDispatch } from 'react-redux'
 
 const CartItem = ({ id, img, title, price, amount }) => {
@@ -11,22 +11,22 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
-        <button
-          className='remove-btn'
-          onClick={() => dispatch(removeItem(id, amount))}
-        >
+        <button className='remove-btn' onClick={() => dispatch(removeItem(id))}>
           remove
         </button>
       </div>
       <div>
         <button
           className='amount-btn'
-          onClick={() => dispatch(increase(amount))}
+          onClick={() => dispatch(increase({ id, amount }))}
         >
           <ChevronUp />
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn' onClick={() => dispatch(decrease(id))}>
+        <button
+          className='amount-btn'
+          onClick={() => dispatch(decrease({ id, amount }))}
+        >
           <ChevronDown />
         </button>
       </div>
