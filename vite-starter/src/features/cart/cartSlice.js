a6ef6ddb0d2ba0d85a983bad19baff81c7bd361a
thenatural86 <philip.smalls86@gmail.com>
2023-09-11 16:51:19 -0400

    remove item

--- vite-starter/src/features/cart/cartSlice.js
+++ vite-starter/src/features/cart/cartSlice.js
@@ -15,7 +15,22 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = []
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount - 1
+    },
+    calculateTotals: (state) => {
+      let amount = 0
+    },
   },
 })
-export const { clearCart } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
 export default cartSlice.reducer
